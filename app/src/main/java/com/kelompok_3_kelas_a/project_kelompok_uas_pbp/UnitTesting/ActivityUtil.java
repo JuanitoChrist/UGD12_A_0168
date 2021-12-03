49690809af4cc8e322c2b6b22ebedff64aded8ba
ASUS <juanitochristian@gmail.com>
2021-12-03 13:17:33 +0700

    tambah kode activity util

--- app/src/main/java/com/kelompok_3_kelas_a/project_kelompok_uas_pbp/UnitTesting/ActivityUtil.java
+++ app/src/main/java/com/kelompok_3_kelas_a/project_kelompok_uas_pbp/UnitTesting/ActivityUtil.java
@@ -1,2 +1,18 @@
-package com.kelompok_3_kelas_a.project_kelompok_uas_pbp.UnitTesting;public class ActivityUtil {
+package com.kelompok_3_kelas_a.project_kelompok_uas_pbp.UnitTesting;
+
+import android.content.Context;
+import android.content.Intent;
+import com.kelompok_3_kelas_a.project_kelompok_uas_pbp.email.RegisterActivity;
+
+
+
+public class ActivityUtil {
+
+    private Context context;
+    public ActivityUtil(Context context) {
+        this.context = context;
+    }
+    public void startRegisterActivity() {
+        context.startActivity(new Intent(context, RegisterActivity.class));
+    }
 }
