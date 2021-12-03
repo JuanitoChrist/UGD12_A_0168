33952c1a9a0cd3232c8760e41063012b10693048
asus <190710070@students.uajy.ac.id>
2021-12-03 11:11:10 +0700

    New one

---
+++ app/src/main/java/com/kelompok_3_kelas_a/project_kelompok_uas_pbp/api/LoginApi.java
@@ -0,0 +1,7 @@
+package com.kelompok_3_kelas_a.project_kelompok_uas_pbp.api;
+
+public class LoginApi {
+    public static final String BASE_URL = "https://uas-pbp.mac-resto.com/api/";
+
+    public static final String ADD_URL = BASE_URL + "login";
+}
---
+++ app/src/main/java/com/kelompok_3_kelas_a/project_kelompok_uas_pbp/preferences/userPreferences.java
@@ -0,0 +1,31 @@
+package com.kelompok_3_kelas_a.project_kelompok_uas_pbp.preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.kelompok_3_kelas_a.project_kelompok_uas_pbp.models.PenggunaModels;
+
+public class userPreferences {
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    Context context;
+
+    public static final String KEY_ID = "id";
+
+    public userPreferences(Context context){
+        this.context = context;
+        sharedPreferences = context.getSharedPreferences("userPreferences", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+    }
+
+    public void setLogin(Long id){
+        editor.putLong(KEY_ID, id);
+    }
+
+    public PenggunaModels getPenggunaModels(){
+        Long id;
+
+        id = sharedPreferences.getLong(KEY_ID, 0);
+        return new PenggunaModels(id);
+    }
+}
