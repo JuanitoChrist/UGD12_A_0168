66eebf70e7fa02b59692647e12ee63e2382adefd
ASUS <juanitochristian@gmail.com>
2021-12-03 13:37:36 +0700

    register class

--- app/src/main/java/com/kelompok_3_kelas_a/project_kelompok_uas_pbp/UnitTesting/RegisterView.java
+++ app/src/main/java/com/kelompok_3_kelas_a/project_kelompok_uas_pbp/UnitTesting/RegisterView.java
@@ -1,4 +1,19 @@
 package com.kelompok_3_kelas_a.project_kelompok_uas_pbp.UnitTesting;
 
 public interface RegisterView {
+
+        String getNama();
+        void showNamaError(String message);
+        String getUmur();
+        void showUmurError(String message);
+        String getEmail();
+        void showEmailError(String message);
+        String getJenisKelamin();
+        void showJenisKelaminError(String message);
+        String getPassword();
+        void showPasswordError(String message);
+        void startRegisterActivity();
+
+
+
 }
